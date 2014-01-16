fb34af000e7d5c1a27b60a060722fbd8615b60a9
hazourano <hazouranosan@gmail.com>
2014-01-17 00:23:05 +0800

    added icons

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "StaticPages" do
 	end
 	describe "Home page" do
 		before {visit root_path}
-		let(:heading) { 'Taplah'}
+		let(:heading) { 'Add-Ctrl'}
 		let(:page_title) {''}
 		it_should_behave_like "all static pages"
     	it {should_not have_title('| Home')}
